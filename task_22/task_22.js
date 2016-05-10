6905e73770587f4dcffdbfdfa179e3ca9d8ad529
Boss Pan <xiaopanwudlut@foxmail.com>
2016-05-10 21:30:04 +0800

    任务22

---
+++ task_22/task_22.js
@@ -0,0 +1,77 @@
+(function () {
+    var root = document.getElementById('root'),
+        preBtn = document.getElementById('pre-btn'),
+        inBtn = document.getElementById('in-btn'),
+        postBtn = document.getElementById('post-btn');
+    var traverseResult = [];
+
+    addListener(preBtn, 'click', function () {
+        traverseResult = [];
+        preOrder(root);
+        render();
+    });
+    addListener(inBtn, 'click', function () {
+        traverseResult = [];
+        inOrder(root);
+        render();
+    });
+    addListener(postBtn, 'click', function () {
+        traverseResult = [];
+        postOrder(root);
+        render();
+    });
+
+    function addListener(obj, type, handler) {
+        if (obj.addEventListener) {
+            obj.addEventListener(type, handler, false);
+        } else if (obj.attachEvent()) {
+            obj.attachEvent('on' + type, handler);
+        } else {
+            obj['on' + type] = handler;
+        }
+    }
+
+    //先序遍历
+    function preOrder(node) {
+        if (node !== null) {
+            traverseResult.push(node);
+            console.log(node);
+            preOrder(node.firstElementChild);
+            preOrder(node.lastElementChild);
+        }
+    }
+
+    //中序遍历
+    function inOrder(node) {
+        if (node !== null) {
+            preOrder(node.firstElementChild);
+            traverseResult.push(node);
+            preOrder(node.lastElementChild);
+        }
+    }
+
+    //后序遍历
+    function postOrder(node) {
+        if (node !== null) {
+            preOrder(node.firstElementChild);
+            preOrder(node.lastElementChild);
+            traverseResult.push(node);
+        }
+    }
+
+    //展示遍历过程的函数
+    function render() {
+        var i = 0;
+        traverseResult[i].classList.add('traversing');
+        var timer = setInterval(function () {
+            i++;
+            if (i < traverseResult.length) {
+                traverseResult[i - 1].classList.remove('traversing');
+                traverseResult[i].classList.add('traversing');
+            } else {
+                traverseResult[i - 1].classList.remove('traversing');
+                clearInterval(timer);
+            }
+        }, 500)
+    }
+})();
